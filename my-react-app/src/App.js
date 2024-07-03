e2e0f616dda936057f95083ec77d27a1b7b20e8e
oviedo2522 <oviedo2522@gmail.com>
2024-07-02 22:25:18 -0700

    Finish

---
+++ my-react-app/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleDecrement = () => {
+    setCounter(counter - 1);
+  };
+
+  return (
+    <div className="App">
+      <h1>Super cool counter</h1>
+      <h2>Current count is: {counter} </h2>
+
+      <button onClick={handleIncrement}>+1</button>
+      <button onClick={handleDecrement} disabled={counter <= 0}>-1</button>
+    </div>
+  )
+}
+
+export default App;
